f5e3b7a6a47b29fd47313c27824fa00ec0195319
yky <ykycome00#naver.com>
2018-06-23 17:33:43 +0900

    행렬곱셈

---
+++ src/YangGiYeol/QUIZ_20180623_03.java
@@ -0,0 +1,56 @@
+package YangGiYeol;
+
+public class QUIZ_20180623_03 {
+	public static void main(String[] args) {
+		
+		int[][] arr1 = {
+				{1,4},
+				{3,2},
+				{4,1}
+		};
+		
+		int[][] arr2 = {
+				{3,3},
+				{3,3}
+		};
+		
+		/*int[][] arr1 = {
+				{2,3,2},
+				{4,2,4},
+				{3,1,4}
+		};
+		
+		int[][] arr2 = {
+				{5,4,3},
+				{2,4,1},
+				{3,1,1}
+		};*/
+		
+	/*	 System.out.print(arr1[0][0]*arr2[0][0] + arr1[0][1]*arr2[0][1]+ " , ");
+		 System.out.println( arr1[0][0]*arr2[1][0] + arr1[0][1]*arr2[1][1]);
+		 
+		 System.out.print(arr1[1][0]*arr2[0][0] + arr1[1][1]*arr2[0][1]+ " , ");
+		 System.out.println(arr1[1][0]*arr2[1][0] + arr1[1][1]*arr2[1][1]);
+		 
+		 System.out.print(arr1[2][0]*arr2[0][0] + arr1[2][1]*arr2[0][1]+ " , ");
+		 System.out.println( arr1[2][0]*arr2[1][0] + arr1[2][1]*arr2[1][1]);*/
+		 
+		 int[][] answer = new int[arr1.length][arr1[0].length];
+		 
+		 for(int i=0; i<arr1.length; i++) {
+			 for(int j=0; j<arr2.length; j++) {
+				 answer[i][j] = arr1[i][0]*arr2[j][0] + arr1[i][1]*arr2[j][1]; 
+			 }
+		 }
+		 
+		 
+		 for(int i=0; i<arr1.length; i++) {
+			 for(int j=0; j<arr2.length; j++) {
+				System.out.print(answer[i][j]+" ");
+			 } System.out.println();
+		 }
+		 
+		
+		
+	}
+}
