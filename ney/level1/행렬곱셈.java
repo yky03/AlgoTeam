cc0f8f69abf24045aa730382b5aab8ee58f55255
NightNineNeighbor <dud9203@gmail.com>
2018-06-23 17:12:32 +0900

    행렬곱셈

---
+++ ney/level1/행렬곱셈.java
@@ -0,0 +1,22 @@
+﻿class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        
+        int x = arr1.length;
+		int y = arr2[0].length;
+		
+		int xx = arr1[0].length;
+		
+		int[][] answer = new int[x][y];
+		
+		for(int i = 0 ; i < x; i++) {
+			for(int j = 0; j < y; j++) {
+				for(int n = 0; n <xx ; n++) {
+						answer[i][j] 
+								+= arr1[i][n] 
+										* arr2[n][j];
+				}
+			}
+		}
+        return answer;
+    }
+}
