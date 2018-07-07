e248a5c79ba6fcc92cef54604c6328995a8e3c52
yky <ykycome00#naver.com>
2018-07-07 15:45:16 +0900

    [20180707] 짝수,홀수

--- YangGiYeol/Quiz_20180707_01.java
+++ YangGiYeol/Quiz_20180707_01.java
@@ -2,12 +2,16 @@
 public class Quiz_20180707_01 {
 	public static void main(String[] args) {
 		
-		String s = "try hello world"; //	TrY HeLlO WoRlD
+		String s = "try Hello world"; //	TrY HeLlO WoRlD
 		String temp ="";
 		
-		for(int i=0; i<s.length(); i++) {
-			temp += (i%2 == 0) ? 
-					String.valueOf(s.charAt(i)).toUpperCase() : String.valueOf(s.charAt(i)).toLowerCase();
+		String[] group = s.split(" ");
+		
+		for(int j=0; j<group.length; j++) {
+			for(int i=0; i<group[j].length(); i++) {
+				temp += (i%2 == 0) ? 
+						String.valueOf(group[j].charAt(i)).toUpperCase() : String.valueOf(group[j].charAt(i)).toLowerCase();
+			} if(group.length-1 != j) temp+=" ";
 		}
 		
 		System.out.println(temp);
