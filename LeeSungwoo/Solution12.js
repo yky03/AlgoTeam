ccc2a4068e261881787eaacd6ad5b5e570e2fd28
2dubbing <2dubbing@gmail.com>
2018-07-21 16:49:18 +0900

    modify Solution12.js

--- LeeSungwoo/Solution12.js
+++ LeeSungwoo/Solution12.js
@@ -1,17 +1,19 @@
-
+var dp = [];   
 
 function solution(n) {
     var answer = 0;
-    
+     
     answer = jump(n);
     
     return answer;
 }
 
 function jump(n) {
-	if (n === 1) return 1;
-	else if (n === 2) return 2;
-	return jump(n - 1) + jump(n - 2);
+	if (dp[n] !== undefined) return dp[n];
+	if (n == 0) return 0;
+	else if (n == 1) return dp[n] = 1;
+	else if (n == 2) return dp[n] = 2;
+	return dp[n] = (jump(n - 1) + jump(n - 2)) % 1234567;
 }
 
 console.log(solution(1000));
