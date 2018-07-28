60d911cb73cb3701faffcc4bc356d63ee0c4b4b1
hyun <codepawn@icloud.com>
2018-07-28 17:02:23 +0900

    수정본

--- Hyun/coin01.py
+++ Hyun/coin01.py
@@ -2,13 +2,18 @@
 def solution(NUM_ARR, RESULT):
     import itertools
     answer = []
-    combis = itertools.combinations_with_replacement(NUM_ARR, RESULT)
-    for i in combis:
-        result_sum = sum(i)
-        if result_sum == RESULT:
-            answer.append(result_sum)
-    print(answer)
-    return answer
+
+    for j in range(1, RESULT + 1):
+
+        combis = [*itertools.combinations_with_replacement(NUM_ARR, j)]
+
+        for i in combis:
+            result_sum = sum(i)
+
+            if result_sum == RESULT:
+                answer.append(i)
+
+    return len(answer)
 
 
 print(solution([1, 2, 5], 10))
