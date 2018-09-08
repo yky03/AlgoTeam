e9191a99f31fd8ac2351ea101c3c45af57ad8767
yky03 <34213213+yky03@users.noreply.github.com>
2018-09-08 16:14:11 +0900

    카카오 3번 캐시알고리즘
    카카오 3번 캐시알고리즘

--- YangGiYeol/Quiz_20180908_01.java
+++ YangGiYeol/Quiz_20180908_01.java
@@ -13,26 +13,19 @@ public static final int cache_miss = 5;
 			return cache_miss * cities.length;
 		} else {
 			for(int i=0; i<cities.length; i++) {
-				if(list.size()<cacheSize) {
+				
 					if(list.contains(cities[i].toLowerCase())) { //배열에 값이 존재하면 캐시히트후 배열 해당인덱스 지우고 뒤에 삽입 , 캐시히트 시간 더하기
 						answer+=cache_hit;
 						list.remove(cities[i].toLowerCase());
 						list.add(cities[i].toLowerCase());
 					} else {
 						answer+=cache_miss;
+						if(list.size()>=cacheSize && !list.contains(cities[i].toLowerCase())){
+						list.remove(0);
+						}
 						list.add(cities[i].toLowerCase());
 					}
-				} else {
-					if(list.contains(cities[i].toLowerCase())) { //배열에 값이 존재하면 캐시히트후 배열 해당인덱스 지우고 뒤에 삽입 , 캐시히트 시간 더하기
-						answer+=cache_hit;
-						list.remove(cities[i].toLowerCase());
-						list.add(cities[i].toLowerCase());
-					} else {
-						answer+=cache_miss;
-						list.remove(0); //캐시초과시 첫번째 인덱스 제거
-						list.add(cities[i].toLowerCase());
-					}
-				}
+				
 			}
 		}
       
